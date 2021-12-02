d79dedac1b038ef0beda60d7149bba4ff75d9545
dadaxonabdullaev <dadaxona7@gmail.com>
2021-12-02 09:04:07 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pagesapp',
+    'accaunts',
+
+
 ]
 
 MIDDLEWARE = [
@@ -121,6 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
+LOGIN_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL='home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accaunts/', include('django.contrib.auth.urls')),
+    path('accaunts/', include('accaunts.urls')),
     path('', include('pagesapp.urls')),
-
 ]
